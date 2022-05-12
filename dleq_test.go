65cc453728934d3783bd85596be3ce6a828e6906
noot <elizabethjbinks@gmail.com>
2022-05-12 12:28:52 -0400

    add failing verify unit test

--- dleq_test.go
+++ dleq_test.go
@@ -12,3 +12,11 @@ func Test_Ed25519_Secp256k1_DLEq(t *testing.T) {
 	err = Ed25519Secp256k1Verify(proof)
 	require.NoError(t, err)
 }
+
+func Test_Ed25519_Secp256k1_DLEq_fail(t *testing.T) {
+	proof, _, err := Ed25519Secp256k1Prove()
+	require.NoError(t, err)
+	proof[1] = 0
+	err = Ed25519Secp256k1Verify(proof)
+	require.NotNil(t, err)
+}
