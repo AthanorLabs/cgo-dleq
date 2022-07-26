7464544cf05f523a6e15a0696cbd3860a7514594
noot <elizabethjbinks@gmail.com>
2022-07-26 00:50:01 -0400

    update gitmodule

--- dleq_test.go
+++ dleq_test.go
@@ -17,6 +17,6 @@ func Test_Ed25519_Secp256k1_DLEq_fail(t *testing.T) {
 	proof, _, err := Ed25519Secp256k1Prove()
 	require.NoError(t, err)
 	proof[1] = 0
-	err = Ed25519Secp256k1Verify(proof)
+	_, _, err = Ed25519Secp256k1Verify(proof)
 	require.NotNil(t, err)
 }
